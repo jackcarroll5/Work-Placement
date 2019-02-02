42875a4d9395fbe724d39b87571c9335334c1a33
Jack Carroll <Jack.Carroll@students@ittralee.ie>
2019-02-02 13:08:10 +0000

    Sample app based on Xamarin tutorials

---
+++ NamePickerApp/NamePickerApp/NamePickerApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NamePickerApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            PickerMain.Items.Add("Mark");
+            PickerMain.Items.Add("Ari");
+            PickerMain.Items.Add("Emmy");
+            PickerMain.Items.Add("Camilla");
+        }
+
+        private void PickerMain_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           var name = PickerMain.Items[PickerMain.SelectedIndex];
+
+            DisplayAlert(name, "Selected Value", "OK");
+        }
+    }
+}
