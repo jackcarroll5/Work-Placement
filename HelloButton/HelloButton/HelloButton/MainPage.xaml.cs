cf66e130f1942cd730002a9f130613b070741bb1
Jack Carroll <Jack.Carroll@students@ittralee.ie>
2019-02-08 12:21:53 +0000

    Extra Xamarin Tutorial Sample Apps - My Version

---
+++ HelloButton/HelloButton/HelloButton/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HelloButton
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void SayHelloButtonOnClicked(object sender, EventArgs e)
+        {
+            var name = NameEntry.Text;
+            await DisplayAlert("Greetings", $"Hello {name}!", "Howdy");
+        }
+
+
+
+    }
+}
