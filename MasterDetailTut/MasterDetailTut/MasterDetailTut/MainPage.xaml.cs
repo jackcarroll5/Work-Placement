c7565774d75f885effa8a5935f0ee840fb5b7b68
Jack Carroll <Jack.Carroll@students@ittralee.ie>
2019-02-08 11:30:21 +0000

    Another Xamarin Tutorial

---
+++ MasterDetailTut/MasterDetailTut/MasterDetailTut/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MasterDetailTut
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Detail = new NavigationPage(new Page1());
+
+            IsPresented = false;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Page1());
+            IsPresented = false;
+        }
+
+        private void Button_Clicked2(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Page2());
+            IsPresented = false;
+        }
+    }
+}
